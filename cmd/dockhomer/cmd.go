8e88961e8bab9f50a9f25eec8353effa283be190
salasberryfin <salash66@gmail.com>
2023-02-07 18:00:48 +0100

    not much time today: creating cli with cobra before going full time into networking and image selection

--- cmd/dockhomer/cmd.go
+++ cmd/dockhomer/cmd.go
@@ -5,7 +5,24 @@ import (
 	"os"
 
 	"github.com/salasberryfin/dockhomer/container"
+	"github.com/spf13/cobra"
 )
+
+type DockhomerOptions struct {
+	Args []string
+	//ConfigFlags
+}
+
+// NewDefaultCommand generates the `dockhomer` command with default arguments
+func NewDefaultCommand() *cobra.Command {
+	return DockhomerOptions{
+		Args: os.Args,
+	}
+}
+
+func runHelp(cmd *cobra.Command) {
+	cmd.Help()
+}
 
 func main() {
 	action, image, command := os.Args[1], os.Args[2], os.Args[3]
