75424bb7a02606046696a602c7794ed784bf03b6
salasberryfin <salash66@gmail.com>
2023-02-13 13:31:40 +0100

    Add CLI built with cobra

--- cmd/dockhomer/cmd.go
+++ cmd/dockhomer/cmd.go
@@ -1,41 +1,47 @@
+/*
+Create a cobra CLI tool for interaction with Dockhomer
+
+Only basic commands are implemented:
+- Open an interactive shell to a container from image: dockhomer run <image>
+- Run a single command in a container from image: dockhomer run command <image> <command>
+*/
 package main
 
 import (
-	"log"
-	"os"
+	"fmt"
 
 	"github.com/salasberryfin/dockhomer/container"
 	"github.com/spf13/cobra"
 )
 
-type DockhomerOptions struct {
-	Args []string
-	//ConfigFlags
-}
-
-// NewDefaultCommand generates the `dockhomer` command with default arguments
-func NewDefaultCommand() *cobra.Command {
-	return DockhomerOptions{
-		Args: os.Args,
-	}
-}
-
-func runHelp(cmd *cobra.Command) {
-	cmd.Help()
-}
-
 func main() {
-	action, image, command := os.Args[1], os.Args[2], os.Args[3]
-	switch action {
-	case "run":
-		cont := container.New(image, "/tmp/dockhomer")
-		log.Printf("Creating container with id: %d\n", cont.ID)
-		if command == "shell" {
-			cont.OpenShell()
-		} else {
-			cont.RunCmd(os.Args[3:]...)
-		}
-	default:
-		log.Fatalf("No valid command found\n")
+
+	var rootCmd = &cobra.Command{
+		Use:   "dockhomer",
+		Short: "Dockhomer is a simple container creation tool",
 	}
+	var interactiveCmd = &cobra.Command{
+		Use:   "run <image>",
+		Short: "open an interactive shell to a container built from an image",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("dockhomer run", args)
+			cont := container.New(args[0], "/tmp/dockhomer")
+			cont.OpenShell()
+		},
+	}
+	var runCmd = &cobra.Command{
+		Use:   "command <image> <cmd>",
+		Short: "Run a command in a container built from an image",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("dockhomer run with command", args)
+			cont := container.New(args[0], "/tmp/dockhomer")
+			cont.RunCmd(args[1:]...)
+		},
+	}
+
+	rootCmd.AddCommand(interactiveCmd)
+	interactiveCmd.AddCommand(runCmd)
+	rootCmd.Execute()
 }
